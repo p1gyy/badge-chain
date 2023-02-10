ba3720efc5b0858568e6adfe8ced25a9b5e77c3b
Piggy Gaming <87405798+Piggy-Gaming@users.noreply.github.com>
2023-02-09 23:36:41 -0500

    Update chain.lua

--- chain.lua
+++ chain.lua
@@ -1,14 +1,13 @@
-local gameId = game.Name
-print("ok")
+local gameNum = game.Name
 
 game.Players.PlayerAdded:Connect(function(plr)
 	--do httpservice stuff
 	local httpService = game:GetService("HttpService")
-	local response = httpService:GetAsync("https://pastebin.com/raw/aQ5mzxeF")
-	local decoded = game:GetService("HttpService"):JSONDecode(response)
+	local games = httpService:GetAsync("https://github.com/Piggy-Gaming/badge-chain/blob/9c1eabff72bcdd397f99b51e53669e43907dd278/games.json")
+	local gamesDecoded = game:GetService("HttpService"):JSONDecode(games)
 
 	local badgeService = game:GetService("BadgeService")
-	local badges = httpService:GetAsync("https://roapi.piggygaming.repl.co/getbadges?id=" .. tostring(game.GameId))
+	local badges = httpService:GetAsync("https://badgechain.piggygaming.repl.co/getbadges?id=" .. tostring(game.GameId))
 	local badgesDecoded = httpService:JSONDecode(badges)
 
 	local badgeCount = 0
@@ -18,7 +17,7 @@ game.Players.PlayerAdded:Connect(function(plr)
 	
 	--award badges
 	local gui = plr.PlayerGui:WaitForChild("MainGUI"):WaitForChild("Background")
-	gui.gameCount.Text = "Game: " .. gameId .. "/" .. decoded["gamesAmount"]
+	gui.gameCount.Text = "Chain: " .. gameNum
 
 	for i = 1, #badgesDecoded do
 		local badgeid = badgesDecoded[i]["id"]
@@ -33,7 +32,9 @@ game.Players.PlayerAdded:Connect(function(plr)
 	
 	--teleport plr
 	gui.badgeCount.Text = "Teleporting..."
-	print("Teleporting to placeid " .. decoded["games"][tostring(gameId + 1)])
-	game:GetService("TeleportService"):Teleport(decoded["games"][tostring(gameId + 1)], plr)
+	local nextgame = gamesDecoded[tostring(gameNum + 1)]
+	print("Teleporting to placeid " .. nextgame
+	game:GetService("TeleportService"):Teleport(nextgame, plr)
 	
 end) 
+print("script loaded")
